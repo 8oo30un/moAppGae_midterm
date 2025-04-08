92a516ab2a8bcacd23b74e7d3c7641e215b8780a
woo-hyun <kwh77974481@gmail.com>
2025-04-08 13:32:11 +0900

    fix: signup page

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,18 +1,98 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: const Center(
-        child: Text(
-          '회원가입 페이지입니다.',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.person_add, size: 64.0),
+                SizedBox(height: 16.0),
+                Text('Create Account', style: TextStyle(fontSize: 24)),
+              ],
+            ),
+            const SizedBox(height: 60.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email Address',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                ),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: const InputDecoration(
+                labelText: 'Confirm Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                ),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24.0),
+            OverflowBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _emailController.clear();
+                    _passwordController.clear();
+                    _confirmPasswordController.clear();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('SIGN UP'),
+                  onPressed: () {
+                    Navigator.pop(context); // 로그인 페이지로 돌아가기
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
